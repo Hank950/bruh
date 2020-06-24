f699554a2a1391755e4f71ee20c0e16b4291d3df
Hank950 <64141042+Hank950@users.noreply.github.com>
2020-06-25 01:34:48 +0300

    Add files via upload

---
+++ ConsoleApplication4/ConsoleApplication4.cpp
@@ -0,0 +1,49 @@
+﻿#include<iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(0, "");
+
+	int i, j;
+	const int ROWS = 4;
+	const int COLS = 9;
+	int mas[ROWS][COLS];
+	
+	for (int i = 0; i < ROWS; i++)
+	{
+		for (int j = 0; j < COLS; j++)
+		{
+			mas[i][j] = rand() % 20;
+		}
+	}
+
+	for (int i = 0; i < ROWS; i++)
+	{
+		for (int j = 0; j < COLS; j++)
+		{
+			cout << mas[i][j]<< " ";
+		}
+		cout << endl;
+	}
+	
+	int max = mas[i][j];
+	for (i=0; i<ROWS; i++)
+	{
+		for (j = 0; j < COLS; j++)
+		{
+			cout << mas[i][j];
+		}
+	}
+
+	for (i = 0; i < ROWS; i++)
+	{
+		for (j = 0; j < COLS; j++)
+		{
+			if (mas[i][j] > max) max = mas[i][j];
+		}
+	}
+
+	cout << "max = " << max << endl;
+	return 0;
+}
